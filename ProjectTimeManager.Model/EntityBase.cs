dec2ebe1610a398220379629a3508c07b90ed3a5
MKrsic <krsicmatija@gmail.com>
2017-07-05 17:56:32 +0200

    Dodani .DAL i .Model

---
+++ ProjectTimeManager.Model/EntityBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectTimeManager.Model
+{
+    public abstract class EntityBase
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; }
+
+        public Nullable<DateTime> UpdatedAt { get; set; }
+    }
+}
